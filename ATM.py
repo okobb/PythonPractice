04373a8fd5b1e957fab1f389882afef18033cd1b
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-15 17:51:40 +0300

    Added second choice and fixed a typo in the menu (number 4).

--- ATM.py
+++ ATM.py
@@ -8,10 +8,21 @@ while choice != 4:
         1. Check Balance
         2. Deposit Money
         3. Withdraw Money
-        3. Exit
+        4. Exit
   """)
   
   choice = int(input("Your choice: "))
   
-  if choice ==1:
+  if choice == 1:
       print("Your balance is: " + str(balance))
+      
+  elif choice == 2:
+    money = int(input("Enter the amount you want to add (Must be positive): "))
+    if(money) < 0:
+        print("You have entered an invalid number.")
+    else:
+        balance  += money
+        print("Money added successfully.")
+  
+  else:
+      print("Invalid Choice.")
