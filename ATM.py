636b4329eaeb3c4b178d6782f075d715812f8218
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-15 17:43:17 +0300

    Added first choice and adjust the menu.

--- ATM.py
+++ ATM.py
@@ -4,9 +4,14 @@ choice = 0;
 while choice != 4:
   # Prompt
   print("""
-        Welcome to the ATM, input your choice:
+        Welcome to the ATM, enter any of the following numbers:
         1. Check Balance
         2. Deposit Money
         3. Withdraw Money
         3. Exit
   """)
+  
+  choice = int(input("Your choice: "))
+  
+  if choice ==1:
+      print("Your balance is: " + str(balance))
