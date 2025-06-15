57e2ebff902fe1f9920a8e53e85eb640fc9b4fb6
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-15 17:38:15 +0300

    Added The Menu.

---
+++ ATM.py
@@ -0,0 +1,12 @@
+balance = 1000;
+choice = 0;
+
+while choice != 4:
+  # Prompt
+  print("""
+        Welcome to the ATM, input your choice:
+        1. Check Balance
+        2. Deposit Money
+        3. Withdraw Money
+        3. Exit
+  """)
