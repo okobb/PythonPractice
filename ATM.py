edb572344ea4b86984a4c0cf664385c264b896bc
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-15 18:27:54 +0300

    fixed the withdrawal section and added a warning for too many withdrawals.

--- ATM.py
+++ ATM.py
@@ -1,6 +1,7 @@
 balance = 1000;
 choice = 0;
 transactions = 0
+warning = 0
 
 while choice != 4:
   # Prompt
@@ -19,21 +20,30 @@ while choice != 4:
       
   elif choice == 2:
     money = int(input("Enter the amount you want to add (Must be positive): "))
-    if(money) < 0:
-        print("You have entered an invalid number.")
-    else:
+    if(money > 0):
         balance  += money
         print("Money added successfully.")
         transactions += 1
+    else:
+        print("You have entered an invalid number.")
         
   elif choice == 3:
     money = int(input("Enter the amount you want to withdraw (Must be positive): "))
-    if(money) < 0:
-        print("You have entered an invalid number.")
+    if(money > 0):
+        if(money > balance):
+            print("You don't have enough money to withdraw the current amount.")
+            warning += 1
+            if(warning > 3):
+                print("You have failed in your withdrawal too many times.")
+                break
+
+        else:
+            balance  -= money
+            print("Money withdrawn successfully.")
+            transactions += 1
     else:
-        balance  -= money
-        print("Money withdrawn successfully.")
-        transactions += 1
+        print("You have entered an invalid number.")
+            
         
   elif choice == 4:
     print("Program ended. Total transactions: "  + str(transactions))
